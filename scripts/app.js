31d961ef6a7fc65e4e9eec0618588763abfd47cc
unknown <sakib3@gmail.com>
2015-07-05 03:13:09 +0200

    added some routes views

--- scripts/app.js
+++ scripts/app.js
@@ -11,4 +11,12 @@ angular
 				url: '/',
 				templateUrl: 'templates/home.html'
 			})
+			.state('about', {
+				url: '/about',
+				templateUrl: 'templates/about.html'
+			})
+			.state('contact', {
+				url: '/contact',
+				templateUrl: 'templates/contact.html'
+			})
 	}])
