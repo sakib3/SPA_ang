159395408f30bf2752919567e054137db6f5e8c8
unknown <sakib3@gmail.com>
2015-07-05 02:57:02 +0200

    define template for ng-view that template is visible between heder and footer and routing options are added

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,14 @@
 // define module app and dependency
 angular
 	.module('app', ['ui.router'])
+	// adding some rotes
+	.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider){
+		//after # whatever written it goes to /
+		$urlRouterProvider.otherwise('/');
+		//save all states in
+		$stateProvider
+			.state('home', {
+				url: '/',
+				templateUrl: 'templates/home.html'
+			})
+	}])
